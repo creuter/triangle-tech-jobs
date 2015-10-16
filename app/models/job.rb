bb92fa0773ea9ec12d5a9b1ea28ce7219bebe6ec
Christian Reuter <christian@thoughtbot.com>
2015-10-16 11:17:43 -0400

    Markup for the new job form

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,6 @@
 class Job < ActiveRecord::Base
+  validates :company, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :url, presence: true
 end
