02fcb484a60a9876150287cfa992fd888287a7aa
Christian Reuter <christian@thoughtbot.com>
2015-10-16 13:12:34 -0400

    Jobs can be edited

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,15 @@
 class JobsController < ApplicationController
   def create
-    @job = Job.new(params.require(:job).permit( :title, :company, :url, :description))
+    @job = Job.new(job_params)
     if @job.save
       redirect_to root_path
     else
       render "new"
     end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
   end
 
   def index
@@ -18,5 +22,20 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to @job
+  end
+
+  def job_params
+    params.require(:job).permit(
+      :title,
+      :company,
+      :url,
+      :description
+    )
   end
 end
